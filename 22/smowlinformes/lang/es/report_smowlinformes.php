662e94b5998c04a3a557fa0ff30a5fcbbfd0ecbb
labayen <mikel.labayen@smowltech.com>
2014-09-22 13:46:03 +0200

    2.2

---
+++ 22/smowlinformes/lang/es/report_smowlinformes.php
@@ -0,0 +1,69 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @package   report_smowlinformes
+ * @author    Manu Fraile Yarza (manuel.fraile@deusto.es)
+ * @copyright Smiley Owl Tech S.L.
+ * @contact   mikel.labayen@smowltech.com
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['error_inf'] = 'ERROR al almacenar los datos';
+$string['error_contact'] = 'Ponerse en contacto con SMOWL';
+$string['error_button'] = 'Regresar al curso';
+$string['pluginname'] = 'SMOWL results';
+$string['plugintitle'] = 'Resultados de SMOWL';
+$string['res_button'] = 'Buscar';
+$string['res_matriculados_list'] = 'Alumnos matriculados';
+$string['res_matriculados_list_help'] = 'Alumnos matriculados en el curso';
+$string['res_min1'] = '5 minutos';
+$string['res_min2'] = '15 minutos';
+$string['res_min3'] = '30 minutos';
+$string['res_min4'] = '60 minutos';
+$string['res_minutes'] = 'Periodo de tiempo';
+$string['res_minutes_help'] = 'Periodo del que visualizar los datos';
+$string['res_modality'] = 'Elegir modalidad';
+$string['res_modality_help'] = 'Modalidad de la que se visualizarán los datos';
+$string['res_modality_type1'] = 'Curso';
+$string['res_modality_type2'] = 'Quiz';
+$string['res_month1'] = 'Enero';
+$string['res_month2'] = 'Febrero';
+$string['res_month3'] = 'Marzo';
+$string['res_month4'] = 'Abril';
+$string['res_month5'] = 'Mayo';
+$string['res_month6'] = 'Junio';
+$string['res_month7'] = 'Julio';
+$string['res_month8'] = 'Agosto';
+$string['res_month9'] = 'Septiembre';
+$string['res_month10'] = 'Octubre';
+$string['res_month11'] = 'Noviembre';
+$string['res_month12'] = 'Diciembre';
+$string['res_months'] = 'Meses del curso';
+$string['res_months_help'] = 'Periodo del que visualizar datos';
+$string['res_no_matriculados'] = 'No hay estudiantes matriculados';
+$string['res_no_quiz'] = 'No se encontraron exámenes';
+$string['res_no_realizados'] = 'Nadie ha completado el quiz';
+$string['res_quiz_list'] = 'Quiz del curso';
+$string['res_quiz_list_help'] = 'Exámenes que forman el curso';
+$string['res_realizados'] = 'Alumnos que han completado el quiz';
+$string['res_realizados_help'] = 'Alumnos que han realizado el quiz';
+$string['res_select_default'] = ' - Seleccionar - ';
+$string['res_settings_header'] = 'Resultados de SMOWL';
+$string['res_title'] = 'Resultados de SMOWL';
